5266f3d3ee79051c497ab34781d8bd20aa80559b
Gaurav Sarkar <60139335+Gaurav7888@users.noreply.github.com>
2022-11-14 05:34:04 +0530

    Add files via upload

---
+++ pages/Forecasting.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.subheader("Holt Winter Model")
+st.image("pages/Holtwinter.png")
+
+st.subheader("Prophet Model")
+
+st.subheader("Sarima Model")
+
+st.subheader("Theta Model")
